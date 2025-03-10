a9e851465de567ac14f8a784148b75c24917278c
KirikV <anoprienkok@gmail.com>
2025-03-10 22:31:28 +0300

    GOTOVO

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    fib()
+}
 
+fun fib() {
+    println("Введите целое число больше 0")
+    var n = readln().toIntOrNull()
+    var i = 1
+    var f1 = 0
+    var f2 = 1
+
+    if (n != null && 0 < n)  {
+        while (i < n) {
+            val sum = f1 + f2
+            f1 = f2
+            f2 = sum
+            i++
+        }
+        println(f2)
+    } else {
+        println("Ошибка ввода")
+        fib()
     }
+}
+//F(2) = F(2-1) + F(2-2)
+
